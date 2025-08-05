f049df3a2e2aae2eafbcd6d113c13473b3c8121a
chrisbrinker@pm.me <chrisbrinker@pm.me>
2025-08-05 18:16:05 +0200

    validate route as post-only

--- server/api/validate.post.ts
+++ server/api/validate.post.ts
@@ -1,27 +1,18 @@
 import { defineEventHandler } from "#imports";
 import PTCGDeckValidator from "../util/PTCGValidator.js";
 export default defineEventHandler(async (event) => {
-  const requestMethod = event.req.method;
-
-  const repo = useNitroApp().cardRepository;
-  if (!repo) {
+  const cardRepo = useNitroApp().cardRepository;
+  if (!cardRepo) {
     return {
       statusCode: 500,
       body: { message: "Card repository not initialized" },
     };
   }
 
-  // TODO is there something like a prehandler for this?
-  if (requestMethod !== "POST") {
-    return {
-      statusCode: 405,
-      body: { message: "Method Not Allowed" },
-    };
-  }
-
   const data = await readBody(event);
 
-  const validator = new PTCGDeckValidator();
+  console.info("Validating decklist:", data.decklist);
+  const validator = new PTCGDeckValidator(cardRepo);
   const result = await validator.check(data.decklist);
 
   return {
